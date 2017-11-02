020875f56afb4e848b34e93cf2bb56b92fd17bcf
kiwiyou | C6H6 <kiwiyou.dev@gmail.com>
2017-11-02 19:32:30 +0900

    기본 구현
    /start, /help, /학교검색 구현

---
+++ src/SchoolLunchBot/SchoolLunchBot.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Threading.Tasks;
+using NLog;
+using SchoolFinder;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.ReplyMarkups;
+
+namespace SchoolLunch.Bot
+{
+    internal sealed class SchoolLunchBot
+    {
+        private static readonly ILogger Logger = LogManager.GetCurrentClassLogger();
+
+        private static readonly ITelegramBotClient Client =
+            new TelegramBotClient(ConfigurationManager.AppSettings["BotToken"]);
+
+        private static readonly SchoolSearch SchoolFinder = new SchoolSearch();
+
+        private static readonly Dictionary<int, UserState> UserStates = new Dictionary<int, UserState>();
+        private static readonly Dictionary<int, Regions> TempUserRegion = new Dictionary<int, Regions>();
+
+        private static void Main(string[] args)
+        {
+            Run();
+        }
+
+        private static void Run()
+        {
+            Console.WriteLine("   __                  _       ___       _   \n" +
+                              "  / / _   _ _ __   ___| |__   / __\\ ___ | |_ \n" + 
+                              " / / | | | | '_ \\ / __| '_ \\ /__\\/// _ \\| __|\n" +
+                              "/ /__| |_| | | | | (__| | | / \\/  \\ (_) | |_ \n" +
+                              "\\____/\\__,_|_| |_|\\___|_| |_\\_____/\\___/ \\__|\n" +
+                              "                                             ");
+            Logger.Info("봇 정보를 받아오는 중...");
+            var me = Client.GetMeAsync().Result;
+            Console.Title = $"@{me.Username}";
+
+            Client.OnMessage += BotMessageReceived;
+
+            Client.StartReceiving();
+            Logger.Info("급식봇이 시작되었습니다. Q를 눌러 종료할 수 있습니다.");
+            while (char.ToLower(Console.ReadKey().KeyChar) != 'q')
+            {
+            }
+            Client.StopReceiving();
+        }
+        
+        private static async void BotMessageReceived(object sender, MessageEventArgs messageEventArgs)
+        {
+            var message = messageEventArgs.Message;
+            if (message == null || message.Type != MessageType.TextMessage) return;
+
+            var chatId = message.Chat.Id;
+            var fromId = message.From.Id;
+
+            if (message.Text.StartsWith("/start"))
+            {
+                await Client.SendChatActionAsync(chatId, ChatAction.Typing);
+                await Client.SendTextMessageAsync(chatId, "급식을 검색할 수 있는 봇입니다!" +
+                                                                   "`/help`를 쳐보세요!",
+                    replyToMessageId: message.MessageId, parseMode: ParseMode.Markdown);
+
+                Logger.Info("/start - User {0}", fromId);
+            } else if (message.Text.StartsWith("/help"))
+            {
+                await Client.SendChatActionAsync(chatId, ChatAction.Typing);
+                await Client.SendTextMessageAsync(chatId, "`/학교검색` - 학교 코드를 검색합니다.",
+                    replyToMessageId: message.MessageId, parseMode: ParseMode.Markdown);
+                Logger.Info("/help - User {0}", fromId);
+            } else if (message.Text.StartsWith("/학교검색"))
+            {
+                await RespondSchoolQuery(message);
+                Logger.Info("/학교검색 - User {0}", fromId);
+            }
+            else if (UserStates.TryGetValue(fromId, out UserState state))
+            {
+                switch (state)
+                {
+                    case UserState.RegionRequested:
+                        try
+                        {
+                            var region = SchoolUtil.GetRegion(message.Text);
+                            TempUserRegion.Add(fromId, region);
+                            await RequestSchoolName(message);
+                        }
+                        catch (KeyNotFoundException)
+                        {
+                            await RespondSchoolQuery(message);
+                        }
+                        break;
+                    case UserState.SchoolNameRequested:
+                        try
+                        {
+                            var name = message.Text;
+                            var type = SchoolUtil.GetSchoolType(name);
+                            var candidates = SchoolFinder.SearchSchool(type, TempUserRegion[fromId], name);
+                            await Client.SendChatActionAsync(chatId, ChatAction.Typing);
+                            await Client.SendTextMessageAsync(chatId, $"총 {candidates.Count}곳의 학교를 찾았습니다.",
+                                replyToMessageId: message.MessageId);
+                            foreach (var candidate in candidates)
+                            {
+                                await Client.SendTextMessageAsync(chatId,
+                                    $"`학교명: {candidate.Name}`\n`주소: {candidate.Adress}`\n`코드: {candidate.Code}`",
+                                    ParseMode.Markdown);
+                            }
+                            UserStates.Remove(fromId);
+                        }
+                        catch (ArgumentException)
+                        {
+                            await Client.SendChatActionAsync(chatId, ChatAction.Typing);
+                            await Client.SendTextMessageAsync(chatId, "잘못된 학교명입니다.",
+                                replyToMessageId: message.MessageId);
+                            await RequestSchoolName(message);
+                        }
+                        break;
+                }
+            }
+        }
+
+        private static async Task RespondSchoolQuery(Message message)
+        {
+            var chatId = message.Chat.Id;
+            var fromId = message.From.Id;
+            await Client.SendChatActionAsync(chatId, ChatAction.Typing);
+            var regionKeyboard = new ReplyKeyboardMarkup(Enum.GetValues(typeof(Regions)).Cast<Regions>()
+                .Select(region => new KeyboardButton(SchoolUtil.GetRegionName(region))).ToArray())
+            {
+                OneTimeKeyboard = true,
+                ResizeKeyboard = true
+            };
+            await Client.SendTextMessageAsync(chatId, "지역을 선택해주세요.", replyToMessageId: message.MessageId,
+                replyMarkup: regionKeyboard);
+            
+            if (UserStates.ContainsKey(fromId))
+            {
+                UserStates[fromId] = UserState.RegionRequested;
+                TempUserRegion.Remove(fromId);
+            }
+            else
+                UserStates.Add(fromId, UserState.RegionRequested);
+        }
+
+        private static async Task RequestSchoolName(Message message)
+        {
+            await Client.SendChatActionAsync(message.Chat.Id, ChatAction.Typing);
+            await Client.SendTextMessageAsync(message.Chat.Id, "학교명을 입력해주세요.",
+                replyToMessageId: message.MessageId, replyMarkup: new ForceReply());
+            UserStates[message.From.Id] = UserState.SchoolNameRequested;
+        }
+    }
+
+    public enum UserState
+    {
+        RegionRequested,
+        SchoolNameRequested,
+    }
+}
---
+++ src/SchoolLunchBot/SchoolUtil.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using SchoolFinder;
+
+namespace SchoolLunch.Bot
+{
+    internal static class SchoolUtil
+    {
+        private static Dictionary<Regions, string> _regionName = new Dictionary<Regions, string>
+        {
+            { Regions.Seoul, "서울" },
+            { Regions.Incheon, "인천" },
+            { Regions.Busan, "부산" },
+            { Regions.Gwangju, "광주" },
+            { Regions.Daejeon, "대전" },
+            { Regions.Daegu, "대구" },
+            { Regions.Sejong, "세종" },
+            { Regions.Ulsan, "울산" },
+            { Regions.Gyeonggi, "경기" },
+            { Regions.Kangwon, "강원" },
+            { Regions.Chungbuk, "충북" },
+            { Regions.Chungnam, "충남" },
+            { Regions.Gyeongbuk, "경북" },
+            { Regions.Gyeongnam, "경남" },
+            { Regions.Jeonbuk, "전북" },
+            { Regions.Jeonnam, "전남" },
+            { Regions.Jeju, "제주" }
+        };
+        private static Dictionary<string, Regions> _nameRegion = new Dictionary<string, Regions>
+        {
+            { "서울", Regions.Seoul },
+            { "인천", Regions.Incheon },
+            { "부산", Regions.Busan },
+            { "광주", Regions.Gwangju },
+            { "대전", Regions.Daejeon },
+            { "대구", Regions.Daegu },
+            { "세종", Regions.Sejong },
+            { "울산", Regions.Ulsan },
+            { "경기", Regions.Gyeonggi },
+            { "강원", Regions.Kangwon },
+            { "충북", Regions.Chungbuk },
+            { "충남", Regions.Chungnam },
+            { "경북", Regions.Gyeongbuk },
+            { "경남", Regions.Gyeongnam },
+            { "전북", Regions.Jeonbuk },
+            { "전남", Regions.Jeonnam },
+            { "제주", Regions.Jeju }
+        };
+
+        public static string GetRegionName(Regions region)
+        {
+            return _regionName[region];
+        }
+
+        public static Regions GetRegion(string name)
+        {
+            return _nameRegion[name];
+        }
+
+        public static SchoolTypes GetSchoolType(string schoolName)
+        {
+            if (schoolName.EndsWith("초등학교") || schoolName.EndsWith("초"))
+                return SchoolTypes.Elementary;
+            if (schoolName.EndsWith("중학교") || schoolName.EndsWith("중"))
+                return SchoolTypes.Middle;
+            if (schoolName.EndsWith("고등학교") || schoolName.EndsWith("고"))
+                return SchoolTypes.High;
+            throw new ArgumentException("잘못된 학교명입니다.");
+        }
+    }
+}
